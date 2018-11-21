6cffb1ad4aeec7b98772bdc6dabbc7811396f1fc
Michael Irwin <mike.irwin1@gmail.com>
2018-11-21 16:04:25 +0000

    passes test 3

--- my_functions.rb
+++ my_functions.rb
@@ -7,7 +7,13 @@ end
 def sum_array(numbers)
   total = 0
   for number in numbers
-  total = total + number
-    end
-    return total
+    total = total + number
+  end
+  return total
+end
+
+# find item within an array, return true if present
+
+def find_item(houses, item)
+  houses.include?(item)
 end
