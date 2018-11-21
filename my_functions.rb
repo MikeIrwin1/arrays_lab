adfdabdcb4ff4091f5adae7d865e638beb6c435b
Michael Irwin <mike.irwin1@gmail.com>
2018-11-21 15:49:02 +0000

    passses test 2

--- my_functions.rb
+++ my_functions.rb
@@ -1,3 +1,13 @@
+# Adding array lengths function
 def add_array_lengths(fruits, berries)
   return fruits.length() + berries.length()
+end
+
+# sum_array test
+def sum_array(numbers)
+  total = 0
+  for number in numbers
+  total = total + number
+    end
+    return total
 end
